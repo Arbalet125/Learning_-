c71965563919818b4cbb363b820be3133cbcc9d7
Arbalet <MametovV@gmail.com>
2022-06-28 19:07:27 +0300

    Seminar2_secondTask_complete

--- DZ_Seminar2_26_06_2022/Program.cs
+++ DZ_Seminar2_26_06_2022/Program.cs
@@ -2,7 +2,8 @@
 ///////ЗАДАЧА №1////////////
 ////////////////////////////
 
-/* Console.Write("Веддите трехзначное число: ");
+/* 
+Console.Write("Веддите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int digitOne = (number / 10);
 int digitTwo = (digitOne % 10);
@@ -14,4 +15,38 @@ if (number > 999 || number < 100)
     else
     {
         Console.Write($"Вторая цифра числа {number}: " + digitTwo);
-    } */
+    } 
+*/
+
+////////////////////////////
+///////ЗАДАЧА №2////////////
+////////////////////////////
+
+/* 
+Console.Write("Веддите число: ");
+int originalNumber = Convert.ToInt32(Console.ReadLine());
+int number = originalNumber;
+
+if (number < 10)
+    {
+        Console.WriteLine($"Вы ввели однозначное число, второй цифры числа {originalNumber} не существует");
+        return;
+    }
+
+while (number > 100)
+    {
+        number = number / 10;
+    }
+if (number < 10)
+   {
+        Console.WriteLine($"Вторая (с начала) цифра числа {originalNumber}: " + number);
+    }
+else
+    {
+        while (number >= 10) 
+        {
+            number = number - 10;
+        }
+        Console.WriteLine($"Вторая (с начала) цифра числа {originalNumber}: " + number);    
+    }
+ */
