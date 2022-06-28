e2a0c1957895c64ddc5a8fe5edb9431e2fefb27c
Arbalet <MametovV@gmail.com>
2022-06-28 19:06:20 +0300

    Seminar2_firstTask_complete

--- DZ_Seminar2_26_06_2022/Program.cs
+++ DZ_Seminar2_26_06_2022/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿////////////////////////////
+///////ЗАДАЧА №1////////////
+////////////////////////////
+
+/* Console.Write("Веддите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digitOne = (number / 10);
+int digitTwo = (digitOne % 10);
+
+if (number > 999 || number < 100)
+    {
+        Console.Write("Веддено неверное число");
+    }
+    else
+    {
+        Console.Write($"Вторая цифра числа {number}: " + digitTwo);
+    } */
