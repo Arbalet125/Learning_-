2ce37a406b52c695018a759a71511465f838f061
Arbalet <MametovV@gmail.com>
2022-07-13 22:23:52 +0300

    end second task

--- DZ_seminar_6_10_07_2022/Program.cs
+++ DZ_seminar_6_10_07_2022/Program.cs
@@ -2,7 +2,7 @@
 //////////////////////////////Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-Console.Write("Введите число М: ");
+/* Console.Write("Введите число М: ");
 int mNumber = Convert.ToInt32(Console.ReadLine());
 
 int[] array = new int[mNumber];
@@ -14,4 +14,33 @@ for (int i = 0; i < mNumber; i++)
     if (array[i] > 0) countNumbers ++;
 }
 
-Console.WriteLine($"Среди чисел [{String.Join("; ", array)}] вы ввели {countNumbers} положительных.");
+Console.WriteLine($"Среди чисел [{String.Join("; ", array)}] вы ввели {countNumbers} положительных."); */
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////Задача 41:///////////////////////////////////////////////////////////////////////////////////////////////
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// x = (b2 - b1) / (k1 - k2);
+// y = y = k1 * x + b1;
+
+/* Console.Write($"Введите точку пересечения первой прямой с осью Y: ");
+double firstLinePointCrossWithY = Convert.ToDouble(Console.ReadLine()); //b1
+
+Console.Write($"Введите угловой коэффицент первой прямой: ");
+double firstLineAngularCoefficient = Convert.ToDouble(Console.ReadLine()); //k1
+
+Console.Write($"Введите точку пересечения второй прямой с осью Y: ");
+double secondLinePointCrossWithY = Convert.ToDouble(Console.ReadLine()); //b2
+
+Console.Write($"Введите угловой коэффицент второй прямой: ");
+double secondLineAngularCoefficient = Convert.ToDouble(Console.ReadLine()); //k2
+
+double firstCrossCoordinate = (secondLinePointCrossWithY - firstLinePointCrossWithY) / (firstLineAngularCoefficient - secondLineAngularCoefficient);
+
+double secondCrossCoordinate = (firstLineAngularCoefficient * firstCrossCoordinate) + firstLinePointCrossWithY;
+
+Console.WriteLine($"Прямые пересекуться в точке [{firstCrossCoordinate}; {secondCrossCoordinate}]"); */
