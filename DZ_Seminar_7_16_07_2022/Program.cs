0d78b7f8b9b83392e35bd46f316a596f60c7e6de
Arbalet <MametovV@gmail.com>
2022-07-22 17:33:00 +0300

    complete first task

--- DZ_Seminar_7_16_07_2022/Program.cs
+++ DZ_Seminar_7_16_07_2022/Program.cs
@@ -8,14 +8,33 @@ int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите максимальное число в массиве: ");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное число в массиве: ");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+
+int coeficent = new Random().Next(minNumber, maxNumber); //Коэфициент для того, чтобы получать числа не только в промежутке между -1 и 1
+
+
+
+Random rand = new Random(); // зарезервируем память для случайного числа
+
 double [,] matrix = new double [rows,columns];
 
-for (int i = 0; i < matrix.GetLength(0); i++)
+for (int i = 0; i < matrix.GetLength(0); i++) //заполняем массив
 {
     for (int j = 0; j < matrix.GetLength(1); j++) 
     {
-        matrix[i,j] = new Random().NextDouble();
-        Console.Write(Math.Round(matrix[i,j], 2) + "\t"); 
+        matrix[i,j] = rand.NextDouble()*coeficent;
+    }
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++) // выводим массив на экран
+{
+    for (int j = 0; j < matrix.GetLength(1); j++) 
+    {
+        Console.Write(Math.Round(matrix[i,j], 2) + "\t"); // Math.Round(х, 2) - выводим с двумя числами после запятой
     }
     Console.WriteLine();
 }
